c564bca9008e883107660866a82495e853573ed1
maria lujan <maria.caimmi@davi git config --global user.name maria>
2021-10-19 12:52:05 -0300

    first commit

--- jcueryActividad/app.js
+++ jcueryActividad/app.js
@@ -3,7 +3,7 @@ const apretandoBotones = () => {
     $('#elemento').css("background-color", "red");
 }
 var variable_global = 1;
-const apretandoBotones2 = () => {
+/*const apretandoBotones2 = () => {
    if(variable_global == 1){
         $('.borrarTodo').hide();
         variable_global=0;
@@ -12,9 +12,73 @@ const apretandoBotones2 = () => {
          $('.borrarTodo').show();
         variable_global=1;
         }
+}*/
+const apretandoBotones2 = () => {
+    if ($('.borrarTodo').is(':visible')) {
+        $('.borrarTodo').hide();
+    } else {
+        $('.borrarTodo').show();
+    }
 }
 
 
 const apretandoBotones3 = () => {
     $("#agergandoCositas").css("display", "block");
- }
+}
+
+$(document).ready(function () {
+    $('#colorcitos > button').addClass('btn').addClass('btn-danger');
+
+});
+
+
+
+
+
+
+
+
+
+
+$('.btn').on("click", function () {
+    let num = $(this).html();
+    $('#input').val(`${num}`);
+    $(this).removeClass('btn-danger').addClass('btn-secondary');
+    $('.btn').on("click", function(){
+        $(this).removeClass('btn-secondary').addClass('btn-danger');
+        $('#input').val('');
+    });
+});
+    
+        
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*$(this).on('click', function () {
+    $(this).removeClass('btn-danger').addClass('btn-secondary');
+
+    $(this).on('click', function () {
+        $(this).removeClass('btn-secondary').addClass('btn-danger');
+    })
+})*/
